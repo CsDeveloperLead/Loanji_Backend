a45f35624e915e510fff9a678838e4ff88a62218
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-12-02 17:20:50 +0530

    admin and blogs backend done

---
+++ src/model/blogs.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const blogsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    tag: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    }
+},
+    {
+        timestamps: true
+    }
+);
+
+export const Blog = mongoose.model("Blog", blogsSchema)
