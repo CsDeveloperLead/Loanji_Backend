5a60daefb3bad547471ab3d2b3abfed95e5508cb
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-12-25 17:21:09 +0530

    back to normal

--- src/app.js
+++ src/app.js
@@ -21,8 +21,7 @@ app.use(
 );
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
-// app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static('public'))
 
 
 //import router here
